11271c4beb157797bc6c7b495aaa83fc118c4c0e
Admin <m.donskoi9@gmail.com>
2015-11-01 21:42:39 +0200

    Rectangle Completed

--- src/Rectangle/Rectangle.java
+++ src/Rectangle/Rectangle.java
@@ -6,13 +6,36 @@ package Rectangle;
 public class Rectangle {
 
     private int width, height;
-    private static final String NAME = "Прямоушольник";
+    private static final String NAME = "Прямоугольник";
 
     public Rectangle() {
 
         width = 10;
         height = 5;
+    }
+    public Rectangle(int width, int height){
+        this.width = width;
+        this.height = height;
+    }
+    public static String getName(){
+        return NAME;
+    }
+    public int getHeight(){
+        return height;
+    }
+    public void setHeight(int h){
+        height = h;
+    }
+    public int getWidth(){
+        return width;
+    }
+    public void setWidth(int w){
+        width = w;
+    }
 
+    public void combine(Rectangle r){
+        width += r.width;
+        height += r.height;
     }
 }
 
---
+++ src/Rectangle/RectangleDemo.java
@@ -0,0 +1,8 @@
+package Rectangle;
+
+/**
+ * Created by Admin on 01.11.2015.
+ */
+public class RectangleDemo {
+
+}
