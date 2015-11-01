03b923304e21394439e1e1ebc286ac724ec22ef2
mikhail <m.donskoi9@gmail.com>
2015-11-01 18:53:44 +0200

    Java OOP

---
+++ src/Rectangle/Rectangle.java
@@ -0,0 +1,19 @@
+package Rectangle;
+
+/**
+ * Created by mikhail on 10/29/15.
+ */
+public class Rectangle {
+
+    private int width, height;
+    private static final String NAME = "Прямоушольник";
+
+    public Rectangle() {
+
+        width = 10;
+        height = 5;
+
+    }
+}
+
+
