9411c64cfa3a66d5c22ef809b53f2aa9b995f38c
Admin <m.donskoi9@gmail.com>
2015-11-01 22:01:27 +0200

    Rectangle Completed

--- src/Rectangle/RectangleDemo.java
+++ src/Rectangle/RectangleDemo.java
@@ -5,4 +5,24 @@ package Rectangle;
  */
 public class RectangleDemo {
 
+    public static void main(String args[]){
+
+        Rectangle r1 = new Rectangle();
+        Rectangle r2 = new Rectangle(3, 2);
+
+        System.out.println(Rectangle.getName());
+
+        System.out.println("r1: " + r1.getHeight() + " x " +r1.getWidth());
+        System.out.println("r2: " + r2.getHeight() + " x " +r2.getWidth());
+
+        r2.setHeight(22);
+        r2.setWidth(33);
+
+        System.out.println("r2: " + r2.getHeight() + " x " +r2.getWidth());
+
+        r1.combine(r2);
+        System.out.println("r1: " + r1.getHeight() + " x " +r1.getWidth());
+
+    }
+
 }
