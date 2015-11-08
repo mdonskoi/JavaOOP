d8cdee4839a98b63fbb81b893fe8dd5d6cc24b71
mikhail <m.donskoi9@gmail.com>
2015-11-08 18:05:00 +0200

    import Phone and Student

---
+++ src/Phone/MainClass.java
@@ -0,0 +1,30 @@
+
+package Phone;
+
+import Phone.Nokia3310;
+
+public class MainClass {
+
+	public static void main(String[] args) {
+		// Phone p = new Phone(); ������ �.�. ����� �����������
+		
+		Nokia3310 nokia = new Nokia3310();
+		System.out.println("Nokia3310 screent size: " + nokia.getScreenSize());
+		nokia.call("123-45-67");
+		nokia.sendSMS("567-78-89", "text message");
+		
+		System.out.println("----------------------------------");
+		
+		IPhone iphone = new IPhone();
+		System.out.println("IPhone screent size: " + iphone.getScreenSize());
+		iphone.call("123-45-67");
+		iphone.sendSMS("567-78-89", "text message");
+		
+		System.out.println("----------------------------------");
+		
+		IPhone5 iphone5 = new IPhone5();
+		System.out.println("IPhone screent size: " + iphone5.getScreenSize());
+		iphone5.call("123-45-67");
+		iphone5.sendSMS("567-78-89", "text message");
+	}
+}
---
+++ src/Student/MyClass.java
@@ -0,0 +1,18 @@
+package Student;
+
+import Student.StudentList;
+
+import java.util.Date;
+
+public class MyClass {
+
+	public static void main(String[] args) {
+		StudentList sl = new StudentList();
+		
+		sl.add(new Student("Seva", "Evgienko", new Date(1986, 1, 1)));
+		sl.add(new Student("Vasya", "Pupkin", new Date(1970, 3, 28)));
+		
+		int n = sl.find("Seva");
+		System.out.println(sl.get(n).getBirth().toString());
+	}
+}
